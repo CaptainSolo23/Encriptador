1e64cbd4db2ae4f57bceba6a6ab68e100ca8f2b7
Ivan Arellano <captainsolo23@gmail.com>
2023-04-20 18:26:11 -0700

    logica-done

--- index.js
+++ index.js
@@ -11,16 +11,38 @@ La letra "u" es convertida para "ufat"
 
   */
 
-let word = 'perre'
-const eKey = 'enter'
+let word = 'fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!'
 
-function sustitution (string){
-    string.split()
-    for(let i  = 0; i < string.length; i++){
-        if(string[i] === 'e'){
-            string[i] = eKey
-        }
-    }
+function encryptMessage (s){
+    
+  // var phrase = document.getElementById("textToEncrypt").value.toLowerCase();
+
+  var sustitution = s.toLowerCase()
+                    .replace(/e/img, "enter")
+                    .replace(/o/img, "ober")
+                    .replace(/i/img, "imes")
+                    .replace(/a/img, "ai")
+                    .replace(/u/img, "ufat");
+
+  return sustitution
+
 }
 
-console.log(sustitution(word))
+console.log(encryptMessage(word))
+
+function decryptMessage (s){
+    
+  // var phrase = document.getElementById("textToEncrypt").value.toLowerCase();
+
+  var sustitution = s.toLowerCase()
+                    .replace(/enter/img, "e")
+                    .replace(/ober/img, "o")
+                    .replace(/imes/img, "i")
+                    .replace(/ai/img, "a")
+                    .replace(/ufat/img, "u");
+
+  return sustitution
+
+}
+
+console.log(decryptMessage(word))
